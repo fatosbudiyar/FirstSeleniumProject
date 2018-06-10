2a57af4dd9cd6abbecc0ff1e06680dc1439d4655
fatosbudiyar <fatosbudiyar@gmail.com>
2018-06-10 04:11:33 -0400

    add amazon

--- src/com/cbt/AmazonSearch.java
+++ src/com/cbt/AmazonSearch.java
@@ -3,12 +3,15 @@ package com.cbt;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumDemo {
+public class AmazonSearch {
+
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/Users/fatosbudiyar/Documents/selenium dependencies/drivers/chromedriver");
+System.setProperty("webdriver.chrome.driver", "/Users/fatosbudiyar/Documents/selenium dependencies/drivers/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://gmail.com");
+		driver.get("http://amazon.com");
 	}
 
-}
+	}
+
+
